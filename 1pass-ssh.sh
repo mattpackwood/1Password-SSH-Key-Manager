95ba3e9311691488c9cba35244295908c7ed25ef
Matt Packwood <mattpackwood@gmail.com>
2020-09-12 15:12:09 -0400

    Create 1pass-ssh.sh

---
+++ 1pass-ssh.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+## Start from here, need to read input, these should be
+## -h display help
+## -r read existing ssh key files from ~/.ssh and upload to 1Password as "Documents"
+## -w write exising 1Password "Documents" to the ~/.ssh directory as ssh key files (and fix any permissions, etc).
+
+
