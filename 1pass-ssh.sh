ff658f3ef73f5d1b0c5f3e5ec0f71276d3a29835
Matt Packwood <mattpackwood@gmail.com>
2020-09-15 10:45:37 -0400

    Update 1pass-ssh.sh

--- 1pass-ssh.sh
+++ 1pass-ssh.sh
@@ -5,4 +5,11 @@
 ## -r read existing ssh key files from ~/.ssh and upload to 1Password as "Documents"
 ## -w write exising 1Password "Documents" to the ~/.ssh directory as ssh key files (and fix any permissions, etc).
 
+##  Commands I will need to use:
+op create document .ssh/id_rsa --title SSH_Private
+op create document .ssh/id_rsa.pub --title SSH_Public
+
+op get document SSH_Private
+
+
 
