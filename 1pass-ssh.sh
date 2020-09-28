cd037d3fb21dd67096e68e3e67d78d435c98aded
Matt Packwood <mattpackwood@gmail.com>
2020-09-28 18:50:03 -0400

    Added an argument processor

--- 1pass-ssh.sh
+++ 1pass-ssh.sh
@@ -1,9 +1,56 @@
-#!/bin/sh
+#!/bin/bash
 
 ## Start from here, need to read input, these should be
 ## -h display help
 ## -r read existing ssh key files from ~/.ssh and upload to 1Password as "Documents"
 ## -w write exising 1Password "Documents" to the ~/.ssh directory as ssh key files (and fix any permissions, etc).
+
+POSITIONAL=()
+while [[ $# -gt 0 ]]
+do
+key="$1"
+
+case $key in
+    -e|--extension)
+    EXTENSION="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -s|--searchpath)
+    SEARCHPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    -l|--lib)
+    LIBPATH="$2"
+    shift # past argument
+    shift # past value
+    ;;
+    --default)
+    DEFAULT=YES
+    shift # past argument
+    ;;
+    *)    # unknown option
+    POSITIONAL+=("$1") # save it in an array for later
+    shift # past argument
+    ;;
+esac
+done
+set -- "${POSITIONAL[@]}" # restore positional parameters
+
+echo "FILE EXTENSION  = ${EXTENSION}"
+echo "SEARCH PATH     = ${SEARCHPATH}"
+echo "LIBRARY PATH    = ${LIBPATH}"
+echo "DEFAULT         = ${DEFAULT}"
+echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
+if [[ -n $1 ]]; then
+    echo "Last line of file specified as non-opt/last argument:"
+    tail -1 "$1"
+fi
+
+
+
+
 
 ##  Commands I will need to use:
 op create document .ssh/id_rsa --title SSH_Private
