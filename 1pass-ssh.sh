68abcf290a290fac3b81eda2fabbb518cf6f2f2a
Matt Packwood <mattpackwood@gmail.com>
2020-09-29 09:03:39 -0400

    Change to a different parsing method

--- 1pass-ssh.sh
+++ 1pass-ssh.sh
@@ -48,6 +48,20 @@ if [[ -n $1 ]]; then
     tail -1 "$1"
 fi
 
+#!/bin/bash
+
+while [[ "$#" -gt 0 ]]; do
+    case $1 in
+        -d|--deploy) deploy="$2"; shift ;;
+        -u|--uglify) uglify=1 ;;
+        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+    esac
+    shift
+done
+
+echo "Should deploy? $deploy"
+echo "Should uglify? $uglify"
+
 
 
 
