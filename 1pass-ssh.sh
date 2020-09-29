d8d487556093afe8cd3cd18c24c5aff44b72431a
Matt Packwood <mattpackwood@gmail.com>
2020-09-29 09:13:14 -0400

    Testing started

--- 1pass-ssh.sh
+++ 1pass-ssh.sh
@@ -5,72 +5,31 @@
 ## -r read existing ssh key files from ~/.ssh and upload to 1Password as "Documents"
 ## -w write exising 1Password "Documents" to the ~/.ssh directory as ssh key files (and fix any permissions, etc).
 
-POSITIONAL=()
-while [[ $# -gt 0 ]]
-do
-key="$1"
 
-case $key in
-    -e|--extension)
-    EXTENSION="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -s|--searchpath)
-    SEARCHPATH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    -l|--lib)
-    LIBPATH="$2"
-    shift # past argument
-    shift # past value
-    ;;
-    --default)
-    DEFAULT=YES
-    shift # past argument
-    ;;
-    *)    # unknown option
-    POSITIONAL+=("$1") # save it in an array for later
-    shift # past argument
-    ;;
-esac
-done
-set -- "${POSITIONAL[@]}" # restore positional parameters
+#while [[ "$#" -gt 0 ]]; do
+#    case $1 in
+#        -h|--help) help=1; shift ;;
+#        -r|--readfile) readfile=1; shift ;;
+#	-w|--writefile) writefile=1;;
+#        *) echo "Unknown parameter passed: $1"; exit 1 ;;
+#    esac
+#    shift
+#done
 
-echo "FILE EXTENSION  = ${EXTENSION}"
-echo "SEARCH PATH     = ${SEARCHPATH}"
-echo "LIBRARY PATH    = ${LIBPATH}"
-echo "DEFAULT         = ${DEFAULT}"
-echo "Number files in SEARCH PATH with EXTENSION:" $(ls -1 "${SEARCHPATH}"/*."${EXTENSION}" | wc -l)
-if [[ -n $1 ]]; then
-    echo "Last line of file specified as non-opt/last argument:"
-    tail -1 "$1"
-fi
-
-#!/bin/bash
-
-while [[ "$#" -gt 0 ]]; do
-    case $1 in
-        -d|--deploy) deploy="$2"; shift ;;
-        -u|--uglify) uglify=1 ;;
-        *) echo "Unknown parameter passed: $1"; exit 1 ;;
-    esac
-    shift
-done
-
-echo "Should deploy? $deploy"
-echo "Should uglify? $uglify"
+echo "Test"
+#echo "Help? $help"
+#echo "Read? $readfile"
+#echo "Read? $readfile"
 
 
 
 
 
 ##  Commands I will need to use:
-op create document .ssh/id_rsa --title SSH_Private
-op create document .ssh/id_rsa.pub --title SSH_Public
+##op create document .ssh/id_rsa --title SSH_Private
+##op create document .ssh/id_rsa.pub --title SSH_Public
 
-op get document SSH_Private
+##op get document SSH_Private
 
 
 
